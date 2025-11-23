0fbabf73dcafb787eef9645ba9b36aa0b0fb538c
VincentToscas <vincent.toscas@student.uclouvain.be>
2025-11-23 04:01:54 +0100

    Ajout (presque) toutes les fonctionnalites demandee jusqu au TP4

---
+++ server/server.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var session = require('express-session');
+var app = express ();
+var bodyParser = require("body-parser");
+var https = require('https');
+var fs = require('fs');
+
+const port = 8080;
+const default_passwrd = "1234";
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+app.use(express.static('static'));
+
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+app.use(session({
+  secret: "propre123",
+  resave: false,
+  saveUninitialized: true,
+  cookie: { 
+    path: '/', 
+    httpOnly: true, 
+    maxAge: 3600000
+  }
+}));
+app.get('/', (req, res)=> {
+    const dateAjrd = new Date();
+    res.render('index', {date: dateAjrd, username:req.session.username});
+});
+app.get('/report', (req, res) => {
+    res.render('report', { username: req.session.username });
+});
+app.get('/connexion', (req, res) => {
+    res.render('connexion', { username: req.session.username, error: null });
+});
+app.post('/login', (req,res)=> {
+    const { username, password} = req.body;
+    if(password===default_passwrd){
+        req.session.username = username;
+        res.redirect('/');
+    }else{
+        res.render('connexion', {username: null, error: "MDP incorrect"});
+    }
+})
+app.get('/logout', (req, res) => {
+    req.session.destroy(err => {
+        res.redirect('/');
+    });
+});
+
+
+app.listen(port, ()=>{
+    console.log(`Server started at http://localhost:${port}`);
+});
